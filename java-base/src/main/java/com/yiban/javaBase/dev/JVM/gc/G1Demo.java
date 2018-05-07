dc7bd132d7f625691f31aecec0c296d146b1ba7d
基础组-段巍巍 <dww@yiban.cn>
2018-05-07 15:52:51 +0800

    up

--- java-base/src/main/java/com/yiban/javaBase/dev/JVM/gc/G1Demo.java
+++ java-base/src/main/java/com/yiban/javaBase/dev/JVM/gc/G1Demo.java
@@ -6,6 +6,23 @@ package com.yiban.javaBase.dev.JVM.gc;
  * @auther WEI.DUAN
  * @date 2018/1/19
  * @website http://blog.csdn.net/dwshmilyss
+ *
+ * javap -verbose
  */
 public class G1Demo {
+    private static int i;
+    private double d;
+
+    public static void print()
+    {
+
+    }
+
+    private boolean trueOrFalse()
+    {
+        return false;
+    }
+//    public static void main(String[] args) {
+//
+//    }
 }

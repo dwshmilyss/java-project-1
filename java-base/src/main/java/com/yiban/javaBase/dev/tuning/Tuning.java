6800b9cb0421cec6100c7be0caa7bcc6fe940a71
dwshmilyss <dwshmilyss@163.com>
2019-08-30 16:56:18 +0800

    up

--- java-base/src/main/java/com/yiban/javaBase/dev/tuning/Tuning.java
+++ java-base/src/main/java/com/yiban/javaBase/dev/tuning/Tuning.java
@@ -1,11 +1,25 @@
 package com.yiban.javaBase.dev.tuning;
+import	java.io.PrintWriter;
+import	java.io.BufferedWriter;
+import	java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import	java.io.FileReader;
+import	java.io.File;
 
 /**
  * Created by Administrator on 2018/3/7 0007.
  */
 public class Tuning {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("");
+        FileReader reader = null;
+        reader = new FileReader(file);
 
+        BufferedReader bufferedReader = null;
+
+        BufferedWriter bufferedWriter = null;
+
+        PrintWriter printWriter = null;
     }
 
     /**

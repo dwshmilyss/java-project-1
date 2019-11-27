792fd1ff8efb4ec856efb56bb53a26e877c26156
dwshmilyss <dwshmilyss@163.com>
2019-11-27 23:09:40 +0800

    up

---
+++ boot/src/main/java/com/yiban/spring/boot/dev/redis/config/RedisConfig.java
@@ -0,0 +1,11 @@
+package com.yiban.spring.boot.dev.redis.config;
+
+/**
+ * Redis配置类
+ *
+ * @auther WEI.DUAN
+ * @date 2019/11/27
+ * @website http://blog.csdn.net/dwshmilyss
+ */
+public class RedisConfig {
+}

67e27b73bec92b89e7e700885d4a50bb0fe23c58
dwshmilyss <dwshmilyss@163.com>
2020-03-20 19:24:26 +0800

    up

--- boot/src/main/java/com/yiban/spring/boot/dev/jpa/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.yiban.spring.boot.dev.jpa.repository;
-
-import com.yiban.spring.boot.dev.jpa.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-    Product findByName(String productName);
-}

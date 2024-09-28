4de252d57ad3862d0304e452f16a346d87f1a1db
dwshmilyss <dwshmilyss@163.com>
2024-09-28 20:15:21 +0800

    delete spring repeat module

--- spring/spring_kafka/src/main/java/com/yiban/spring/spring_kafka/dev/producer/ProducerService.java
+++
@@ -1,6 +0,0 @@
-package com.yiban.spring.spring_kafka.dev.producer;
-
-public interface ProducerService {
-    void send(String topic, String str, int count);
-    void sendJson(String topic, String json, int count);
-}
